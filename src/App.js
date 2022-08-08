a49c19cbf4e6a8590a1c45a5823b7f2d4402e2ab
igopalakrishnan <kingkrishna809@gmail.com>
2022-08-08 22:10:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <hi>my app</hi>
-    </div>
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text >
+            Home
+          </Navbar.Text>
+          <Navbar.Text>
+            Home
+          </Navbar.Text>
+          <Navbar.Text>
+            Home
+          </Navbar.Text>
+          <Navbar.Text>
+            Home
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 

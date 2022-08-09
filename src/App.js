4b5b7ac69247e50355b40a195e8585633e2a57de
igopalakrishnan <kingkrishna809@gmail.com>
2022-08-09 10:29:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <Navbar>
-      <Container>
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text >
-            Home
-          </Navbar.Text>
-          <Navbar.Text>
-            About
-          </Navbar.Text>
-          <Navbar.Text>
-            Home
-          </Navbar.Text>
-          <Navbar.Text>
-            Home
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Routes>
+      <Route path='/' element={<Navbar />}/>
+    </Routes>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+* @author
+* @function Navbar
+**/
+
+const Navbar = (props) => {
+  return(
+    <div>Navbar</div>
+   )
+
+ }
+
+export default Navbar;
